4b78c48b5001cea01caba1833644de2fdd21a8ee
Mr. Hax ID <37820898+hax0rid@users.noreply.github.com>
2020-06-02 15:29:14 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+/*
+==========================================================
+         _____ _____ _____               __          __ 
+  ____ _/ ___// ___// ___/____ _____    / /_  ____  / /_
+ / __ `/ __ \/ __ \/ __ \/ __ `/ __ \  / __ \/ __ \/ __/
+/ /_/ / /_/ / /_/ / /_/ / /_/ / / / / / /_/ / /_/ / /_  
+\__,_/\____/\____/\____/\__,_/_/ /_(_)_.___/\____/\__/  
+                                                        
+==========================================================
+
+Bot Auto Claim Bonus Cryptoplace.cloud
+Termux :
+pkg upgrade && pkg update
+pkg install php
+pkg install git
+git clone https://github.com/hax0rid/crypto
+cd crypto
+nano config.php
+isikan email dan password Cryptoplace.cloud
+save dan close
+php crypto.php
+
+*/
+
+$email = "emailkamu@gmail.com";
+$password = "passwordkamu";
